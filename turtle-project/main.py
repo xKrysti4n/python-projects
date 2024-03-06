c08bab3703b5781a78f22dec7b0f730b7d761fec
xkrysti4n <krystianbinek1223@gmail.com>
2024-03-06 23:45:05 +0100

    commit

--- turtle-project/main.py
+++ turtle-project/main.py
@@ -1,13 +1,23 @@
+import random
 from turtle import Turtle, Screen
 t = Turtle()
+Screen().bgcolor("black")
+Screen().colormode(255)
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r,g,b)
+    return random_color
 
-for _ in range(4):
-    t.forward(10)
-    t.pu()
-    t.forward(10)
-    t.pd()
-    t.right(90)
 
+t.width(2)
+t.speed("fastest")
+def draw_spirograph(size_of_gap):       
+    for _ in range(int(360 / size_of_gap)):
+        t.color(random_color())
+        t.circle(100)
+        t.setheading(t.heading()+size_of_gap)
 
-    
+draw_spirograph(2)
 Screen().exitonclick()
